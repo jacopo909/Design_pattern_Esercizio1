d3fbd88043e3895e271145161056d36daa9309b3
jacopo909 <60032905+jacopo909@users.noreply.github.com>
2021-03-02 23:28:44 +0100

    Add files via upload

---
+++ Riga.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_1
+{
+    public class Riga
+    {
+        private List<Cella> cella = new List<Cella>();
+        public Riga() { }
+
+        public Cella CreaCella()
+        {
+            Cella c = new Cella();
+            cella.Add(c);
+            return c;
+        }
+    }
+}
---
+++ Tabella.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_1
+{
+    public class Tabella
+    {
+        private List<Riga> riga = new List<Riga>();
+
+        public Riga CreaRighe()
+        {
+            Riga r = new Riga();
+            riga.Add(r);
+            return r;
+        }
+    }
+}
